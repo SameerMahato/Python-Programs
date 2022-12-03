1ec0e497b18d986d0696b515ae9ff3ebdd8d9e9d
Sameer Mahato <112648390+SameerMahato@users.noreply.github.com>
2022-12-03 20:30:45 +0530

    Add files via upload

---
+++ laregstnumber.py
@@ -0,0 +1,13 @@
+#WAP to find the largest number
+
+a=float(input("Enter The First Number ="))
+b=float(input("Enter The Second Number ="))
+c=float(input("Enter The Third Number ="))
+if(a>=b) and (a>=c):
+ print(a ,"is the largest number")
+elif(b>=c) and (b>=a) :
+ print(b ,"is the largest number")
+else:
+ print(c  ,"is the largest number")
+
+ 
