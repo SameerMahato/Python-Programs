579477262e00bf6e8829f888e35c93ecf9f8e0b2
Sameer Mahato <112648390+SameerMahato@users.noreply.github.com>
2022-12-06 21:34:58 +0530

    Add files via upload

---
+++ Cloning.py
@@ -0,0 +1,29 @@
+# data1 =input["enter the first list"]
+# list1 = data1.split(",")
+# data2 =input["enter the second list"]
+# list2 = data2.split(",")
+#
+# if list[0] == list2[0] or list[-1] == list[-1]:
+#     print("True")
+# else:
+#     print("False")
+#
+# data = input("data: ")
+# list1 = data.split(",")
+# list2=[]
+# for i in range(len(list1)):
+#     if list1[i]not in list2:
+#         list2.append(list1[i])
+# print(list1)
+# print("After removing duplicates:",list2)
+
+'''data = input("data: ")
+list1 = data.split(",")
+list2 = []
+for i in range(len(list1)):
+	if list1[i] not in list2:
+		list2.append(list1[i])
+print(list1)
+print("after removing duplicates:", list2)'''
+
+
